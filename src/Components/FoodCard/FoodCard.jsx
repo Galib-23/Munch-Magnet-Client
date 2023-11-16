4fbd153d6148bcabc01572ca147bf472e1c0ec19
Galib-23 <aagalib2323@gmail.com>
2023-11-16 11:38:30 +0600

    React Tabs Installed

---
+++ src/Components/FoodCard/FoodCard.jsx
@@ -0,0 +1,10 @@
+
+const FoodCard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FoodCard;
--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -1,15 +1,32 @@
+import { useState } from 'react';
 import orderImg from '../../../assets/shop/banner2.jpg';
 import Cover from '../../Shared/Cover/Cover';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import useMenu from '../../../hooks/useMenu';
 
 const Order = () => {
+    const [tabIndex, setTabIndex] = useState(0);
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const offered = menu.filter(item => item.category === 'offered');
     return (
         <div>
             <Cover img={orderImg} title={"Order Food"}></Cover>
-            <div className="tabs tabs-lifted">
-                <a className="tab">Tab 1</a>
-                <a className="tab tab-active">Tab 2</a>
-                <a className="tab">Tab 3</a>
-            </div>
+            <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+                <TabList>
+                    <Tab>Salad</Tab>
+                    <Tab>Pizza</Tab>
+                    <Tab>Soup</Tab>
+                    <Tab>Dessert</Tab>
+                    <Tab>Drinks</Tab>
+                </TabList>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+            </Tabs>
         </div>
     );
 };
