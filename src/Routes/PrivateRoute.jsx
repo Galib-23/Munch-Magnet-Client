70aa192e301edd1fd9949e235262452470282afa
Galib-23 <aagalib2323@gmail.com>
2023-11-19 13:58:06 +0600

    Private Route Created

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+    if(loading){
+        return <progress className="progress w-56"></progress>
+    }
+    if(user){
+        return children;
+    }
+    return <Navigate to='/login'></Navigate>
+};
+
+export default PrivateRoute;
